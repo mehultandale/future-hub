2fb0270d8cf81b02ae4294b38ed8a82e0deaf84e
Mehul <mehultandale@gmail.com>
2022-12-25 10:21:03 +0530

    > Adding react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 
 
+import { 
+    createBrowserRouter, 
+    RouterProvider,
+    Link 
+} from "react-router-dom";
 import './index.css';
 
 
-function App() {
+function App1() {
     return (
         <div>
             <h1>FutureHub</h1>
             <h4>Find the gaps in your knowledge of Frontend skills.</h4>
-            <button>TAKE THE FREE TEST</button>
+            <Link to={'/test'} >
+                <button>TAKE THE FREE TEST</button>
+            </Link>
             <p>You could learn something new today and become an even better programmer.</p>
         </div>
     );
+}
+
+
+function Test() {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    );
+}
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App1 />
+    },
+    {
+        path: "/test",
+        element: <Test />
+    }
+]);
+
+
+function App() {
+    return (
+        <RouterProvider router={router} />
+    )
 }
 
 
