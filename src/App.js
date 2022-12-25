e3def11ef80536d0a4ed6d77bc1c9172988126e0
Mehul <mehultandale@gmail.com>
2022-12-25 10:06:55 +0530

    > Adding markup for home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import './index.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>FutureHub</h1>
+            <h4>Find the gaps in your knowledge of Frontend skills.</h4>
+            <button>TAKE THE FREE TEST</button>
+            <p>You could learn something new today and become an even better programmer.</p>
+        </div>
+    );
 }
+
 
 export default App;
