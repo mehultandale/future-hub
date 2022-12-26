cbd9e44e40af709687235d702aa3aff7edc8cbce
Mehul <mehultandale@gmail.com>
2022-12-26 16:47:13 +0530

    > Adding answer options

--- src/App.js
+++ src/App.js
@@ -34,11 +34,25 @@ function Home() {
 }
 
 
+/* Single answer button */
+function AnswerOption(props) {
+    function answerReceived() {
+        console.log('aR', props);
+    }
+    return (
+        <button onClick={answerReceived} >{ props.label }</button>
+    );
+}
+
+
 /* Test (Quiz) page */
 function Test() {
     return (
         <div>
-            <h1>Test</h1>
+            <p>Question 1</p>
+            <AnswerOption label="Compiled" />
+            <AnswerOption label="Interpreted" />
+            <h5>You've learned 1 new thing today, good going!</h5>
         </div>
     );
 }
