288b35f4c14a17b9e0ac0ea91de6a852471074e3
Mehul <mehultandale@gmail.com>
2022-12-26 16:27:27 +0530

    > Adding smaller components

--- src/App.js
+++ src/App.js
@@ -44,11 +44,24 @@ function Test() {
 }
 
 
+/* Single category button */
+function SingleCategory(props) {
+    return (
+        <Link to={'/test'} >
+            <button>{ props.name }</button>
+        </Link>
+    );
+}
+
+
 /* Categories page */
 function ProgrammingLanguages() {
     return (
         <div>
             <MainHeader />
+            <p>I would like to improve on</p>
+            <SingleCategory name="HTML & CSS" />
+            <SingleCategory name="JavaScript" />
         </div>
     );
 }
